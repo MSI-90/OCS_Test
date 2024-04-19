8024afcb580ff4a1422d5912cb24e0b987122595
MSI-90 <s.miller_22@outlook.com>
2024-04-20 00:51:17 +0700

    Небольшой изменение, внедрение зависимости IActivityService мигрировало из когструктора ActivityController в метод действия GetActivities

--- NotionTestWork/Controllers/ActivityController.cs
+++ NotionTestWork/Controllers/ActivityController.cs
@@ -5,13 +5,13 @@ namespace Api.Controllers;
 
 [Route("activities")]
 [ApiController]
-public class ActivityController(IActivityService _service) : ControllerBase
+public class ActivityController : ControllerBase
 {
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-    public IActionResult GetActivities()
+    public IActionResult GetActivities([FromServices] IActivityService _service)
     {
         var result = _service.GetActivities();
         return Ok(result);
